a8ba54a52386889260c5614d5ed54f3001a52448
westernbeat <52187998+westernbeat@users.noreply.github.com>
2020-02-27 21:33:42 +1100

    Delete master.js

--- MyProfile/scripts/master.js
+++
@@ -1,9 +0,0 @@
-var heading1 = document.getElementById('idh1');
-
-heading1.addEventListener('mouseover', function(){
-  heading1.style.opacity = 0.1;
-});
-
-heading1.addEventListener('mouseout', function(){
-  heading1.style.opacity = 1.0;
-});
