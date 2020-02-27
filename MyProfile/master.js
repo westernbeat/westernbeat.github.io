99ca236e13cc9df05257b4a8e735709c7c494d95
westernbeat <52187998+westernbeat@users.noreply.github.com>
2020-02-27 21:44:37 +1100

    Create master.js

---
+++ MyProfile/master.js
@@ -0,0 +1,9 @@
+var heading1 = document.getElementById('idh1');
+
+heading1.addEventListener('mouseover', function(){
+  heading1.style.opacity = 0.1;
+});
+
+heading1.addEventListener('mouseout', function(){
+  heading1.style.opacity = 1.0;
+});
